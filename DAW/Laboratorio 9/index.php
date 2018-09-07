38a9c7a8e4904bd664ab2bdafd5afef05c2f9089
Ana <analup250@gmail.com>
2018-09-07 03:07:43 +0000

    Agregar html

--- DAW/Laboratorio 9/index.php
+++ DAW/Laboratorio 9/index.php
@@ -1,12 +1,13 @@
 
 <?php
 
-function average(){
+$array = array(9, 6, 3, 7, 2, 6, 5, 8, 1);
+
+function average($array){
     
-    $array = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     echo $array;
     
-    for ($i = 0 ; $i < 10 ; $i++){
+    for ($i = 0 ; $i < 9 ; $i++){
         
         $aux = $aux + $array[i];
     }
@@ -15,5 +16,89 @@ function average(){
 }
 
 
+function median($array){
+    
+    echo $array;
+
+    echo $array[CEILING(9/2)];
+    
+}
+
+function BUBBLESORT($array){
+    for ($i = 0 ; 9 ; $i++){
+        for($j = 0 ; $i - 1 ; $j++){
+            if($array[j] > $array[j + 1]){
+                $aux = $array [j+1];
+                $array[j + 1] = $array[j];
+                $array[j] = $aux;
+                
+            }
+        }
+    }
+}
+
+function BBUBBLESORT($array){
+    for ($i = 0 ; 9 ; $i++){
+        for($j = 0 ; $i - 1 ; $j++){
+            if($array[j] < $array[j + 1]){
+                $aux = $array [j+1];
+                $array[j + 1] = $array[j];
+                $array[j] = $aux;
+                
+            }
+        }
+    }
+}
+
+function operations($array){
+    
+    echo $array;
+    echo "<ul> <li>Promedio: " . average($array) . "</li> <li> Mediana: " . median($array) . "</li> <li>Menos a más: " . BUBBLESORT($array) . "</li><li>Más a menos: " . BBUBBLESORT($array). "</li></ul>";
+    
+}
+
+function cuacu(){
+    $n = 5;
+    echo "<table> <tr> <th>números</th> ";
+    for( $i = 0 ; $i < n; $i++){
+        echo "<td>" . $i ."</td>";
+    }
+    echo"</tr>";
+    
+    echo "<table> <tr> <th>Cuadrados</th> ";
+    for( $i = 0 ; $i < n; $i++){
+        echo "<td>" . $i*$i ."</td>";
+    }
+    echo"</tr>";
+    
+    echo "<table> <tr> <th>números</th> ";
+    for( $i = 0 ; $i < n; $i++){
+        echo "<td>" . $i*$i*$i ."</td>";
+    }
+    echo"</tr>";
+}
+
+function vanyaAndFence(){
+    
+    $n = 3;
+    $h = 7;
+    $aux = 0;
+    $friends = array(4, 5, 14);
+    
+    echo "Number of friends: " . $n . "height: " . $h . "Friend's height: " . $friends;
+    
+    for ($i = 0 ; $i < 3 ; $i++){
+        
+        if ($friends[i] > $n){
+            $aux = $aux + 2;
+        }
+        else {
+            $aux = $aux +1;
+        }
+    }
+    echo $aux;
+    
+    
+}
 
 ?>
