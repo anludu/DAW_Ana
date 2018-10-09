b0aa05a12c434476029dad56ca958878a17ff28c
Ana Lucía Dueñas Chávez <a01702080@itesm.mx>
2018-10-09 12:25:49 -0500

    Entregar Laboratorio 10

---
+++ BDD/Laboratorio 10/Laboratorio10.sql
@@ -0,0 +1,69 @@
+CREATE TABLE Materiales
+(
+Clave numeric(5), 
+Descripcion varchar(50),
+Costo numeric(8,2)
+)
+
+CREATE TABLE Proveedores
+(
+RFC char(13), 
+RazonSocial varchar(50),
+)
+
+CREATE TABLE Proyectos
+(
+Numero numeric(5), 
+Denominacion varchar(50),
+)
+
+
+
+CREATE TABLE Entregan
+(
+Clave numeric(5), 
+RFC char(13),
+Numero numeric(5),
+Fecha datetime,
+Cantidad numeric(8,2)
+)
+
+
+
+BULK INSERT a1702080.a1702080.[Proyectos]
+FROM 'e:\wwwroot\a1702080\proyectos.csv'
+WITH(
+	CODEPAGE = 'ACP',
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '0x0a'
+)
+
+
+SELECT * FROM Entregan
+
+BULK INSERT a1702080.a1702080.[Proveedores]
+FROM 'e:\wwwroot\a1702080\proveedores.csv'
+WITH(
+	CODEPAGE = 'ACP',
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '0x0a'
+)
+
+BULK INSERT a1702080.a1702080.[Materiales]
+FROM 'e:\wwwroot\a1702080\materiales.csv'
+WITH(
+	CODEPAGE = 'ACP',
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '0x0a'
+)
+
+SET DATEFORMAT dmy
+BULK INSERT a1702080.a1702080.[Entregan]
+FROM 'e:\wwwroot\a1702080\entregan.csv'
+WITH(
+	CODEPAGE = 'ACP',
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '0x0a'
+)
+
+SELECT * FROM Materiales
---
+++ DAW/Lab11/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A01702080
+ * Date: 10/4/2018
+ * Time: 8:40 PM
+ */
